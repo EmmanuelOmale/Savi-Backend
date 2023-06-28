e9adda2fc8bba261b78f566771b1f504b714bb3b
Kimbill <ayooluwaa@decagonhq.com>
2023-06-28 06:34:57 +0100

    added files to the folders

--- Savi.Core/Services/Class1.cs
+++ Savi.Core/Services/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Savi.Core.Interfaces
+namespace Savi.Core.Services
 {
     internal class Class1
     {
--- Savi.Core/Utilities/Class1.cs
+++ Savi.Core/Utilities/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Savi.Core.Interfaces
+namespace Savi.Core.Utilities
 {
-    internal class Class1
+    public class Class1
     {
     }
 }
