42da592a783d0a7ebc995d581d9f37131fcb35f4
Sodiq Alabi <alabisdq@gmail.com>
2023-06-29 11:59:36 +0100

    Implemented global exception handling and error logging using serilog

---
+++ Savi.Data/Domains/ErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace Savi.Data.Domains
+{
+    public class ErrorDetails
+    {
+        public string? Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
