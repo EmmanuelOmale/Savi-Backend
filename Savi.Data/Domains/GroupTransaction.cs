08ac0b928413ebe8c6534c05b964df73edc908f8
Signalls <ndubuisi.charles@decagon.dev>
2023-07-03 15:58:08 +0100

    SignUpImplemenation-SAVI-9

--- Savi.Data/Domains/GroupTransaction.cs
+++ Savi.Data/Domains/GroupTransaction.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Savi.Data.Domains
+﻿namespace Savi.Data.Domains
 {
     public class GroupTransaction : BaseEntity
     {
@@ -16,6 +10,6 @@ namespace Savi.Data.Domains
         public string GroupId { get; set; }
         public Group Group { get; set; }
         public string UserId { get; set; }
-        public User User { get; set; }
+        public ApplicationUser User { get; set; }
     }
 }
