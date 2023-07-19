ba41c7c5fcba238078b0d7dfd56eaa089259704a
janyanwu206 <janyanwu206@gmail.com>
2023-07-19 18:30:36 +0100

    Payment

---
+++ Savi.Data/Domains/Wallet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savi.Data.Domains
+{
+    public class Wallet : BaseEntity
+    {
+        public string WalletId { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+        public string Reference { get; set; }
+        public string Pin { get; set; } = null!;
+        public string PaystackCustomerCode { get; set; } = null!;
+        public void SetWalletId(string phoneNumber)
+        {
+            if (phoneNumber.StartsWith("+234"))
+            {
+                phoneNumber = phoneNumber.Substring(4); // Remove '+234'
+            }
+            else if (phoneNumber.StartsWith("0"))
+            {
+                phoneNumber = phoneNumber.Substring(1); // Remove leading '0'
+            }
+
+            if (phoneNumber.Length == 10 && long.TryParse(phoneNumber, out long walletId))
+            {
+                
+                WalletId = walletId.ToString();
+            }
+            else
+            {
+                throw new Exception("Invalid Wallet Id Format");
+            }
+        }
+
+        public ApplicationUser User { get; set; }
+        public int UserId { get; set; }
+    }
+
+}
---
+++ Savi.Data/Domains/WalletFunding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savi.Data.Domains
+{
+    public class WalletFunding : BaseEntity
+    {
+        public decimal Amount { get; set; }
+        public DateTime FundingDate { get; set; }
+
+
+        public int WalletId { get; set; }
+        public Wallet Wallet { get; set; }
+    }
+}
