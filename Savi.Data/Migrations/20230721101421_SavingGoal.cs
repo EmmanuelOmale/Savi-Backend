3f876a8f4c9ea38c3fd7df72e558a56e6e0a068c
Chibueze Amadi <123497071+Elmagn1f1c0@users.noreply.github.com>
2023-07-21 11:21:17 +0100

    Updated

---
+++ Savi.Data/Migrations/20230721101421_SavingGoal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Savi.Data.Migrations
+{
+    public partial class SavingGoal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SavingGoals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GoalName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TargetAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AmountToAddPerTime = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Frequency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavingGoals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavingGoals");
+        }
+    }
+}
